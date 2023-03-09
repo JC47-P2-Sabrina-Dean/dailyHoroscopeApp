7e98f1bdb6f39b8e5116b0a2814ac378d724e6c7
SabrinaIsTheMan <sabrina.yt.man@gmail.com>
2023-03-08 22:06:39 -0500

    ethod for hovers, centered scale for .signRange, added github links to footer, added alt tags inc. variable one for horoscope view

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,5 @@
 const app = {};
 
-app.apiSelection = document.querySelector('.apiSelection');
-app.apiDateRange = document.querySelector('.apiDateRange');
-app.apiCurrentDate = document.querySelector('.apiCurrentDate');
-app.apiDescription = document.querySelector('.apiDescription');
-app.apiCompatibility = document.querySelector('.apiCompatibility');
-app.apiMood = document.querySelector('.apiMood');
-app.apiColor = document.querySelector('.apiColor');
-app.apiLuckyNumber = document.querySelector('.apiLuckyNumber');
-app.apiLuckyTime = document.querySelector('.apiLuckyTime');
-
-app.apiImage = document.getElementById('apiImage');
-
-app.apiURL = "https://aztro.sameerkumar.website";
-
-app.apiSign = ''; // based on user input
-app.apiDay = 'today';
-
 app.getHoroscope = () => {
     const url = new URL(app.apiURL);
     url.search = new URLSearchParams({
@@ -33,6 +16,7 @@ app.getHoroscope = () => {
             console.log(jsonResult);
 
             app.apiImage.attributes.src.textContent = `./assets/${app.apiSign}.png`;
+            app.apiImage.attributes.alt.textContent = `${app.apiSign} symbol`;
 
             app.apiSelection.innerText = app.apiSign;
             app.apiDateRange.innerText = jsonResult.date_range;
@@ -47,96 +31,114 @@ app.getHoroscope = () => {
             app.apiLuckyTime.innerText = jsonResult.lucky_time;
         });
 };
-    
-    // method to change back to the sign button selection screen
-     app.changeSign = () => {
-        app.changeButton.addEventListener('click', function(){
-            console.log("click");
-            app.changeButton.style.opacity = "0";
-            app.horoscopeSection.style.opacity = "0";
 
-            setTimeout(function () {
-                app.horoscopeSection.style.display = 'none';
-                app.changeButton.style.visibility = "hidden";
-                app.signsViewSection.style.display = "block";
-                app.signsViewSection.style.opacity = "1";
-            }, 250);
-        });
-     }
+app.hovers = () => {
+    // method to display sign info when user mouseovers symbol
+    app.signsButtons.forEach((button) => {
+        button.addEventListener('mouseover', function () {
+            let sign = this.attributes.id.textContent;
+            app.signInfo = document.getElementById(`${sign}Info`);
+            app.signInfo.style.transform = 'scale(1, 1)';
+        })
+    });
+
+    // method to hide sign info when user mouseout of symbol
+    app.signsButtons.forEach((button) => {
+        button.addEventListener('mouseout', function () {
+            let sign = (this.attributes.id.textContent);
+            app.signInfo = document.getElementById(`${sign}Info`);
+            app.signInfo.style.transform = 'scale(0)';
+        })
+    });
+
+    // method to display sign info when user focus in on symbol
+    app.signsButtons.forEach((button) => {
+        button.addEventListener('focusin', function () {
+            let sign = this.attributes.id.textContent;
+            app.signInfo = document.getElementById(`${sign}Info`);
+            app.signInfo.style.transform = 'scale(1, 1)';
+        })
+    });
+
+    // method to hide sign info when user focus out on symbol
+    app.signsButtons.forEach((button) => {
+        button.addEventListener('focusout', function () {
+            let sign = (this.attributes.id.textContent);
+            app.signInfo = document.getElementById(`${sign}Info`);
+            app.signInfo.style.transform = 'scale(0)';
+        })
+    });
+}
+
+// method to change back to the sign button selection screen
+app.changeSign = () => {
+    app.changeButton.addEventListener('click', function () {
+        console.log('click');
+        app.changeButton.style.opacity = '0';
+        app.horoscopeSection.style.opacity = '0';
+
+        // gives opacity transition time, then switch views back
+        setTimeout(function() {
+            app.horoscopeSection.style.display = 'none';
+            app.changeButton.style.visibility = 'hidden';
+            app.signsViewSection.style.display = 'block';
+            app.signsViewSection.style.opacity = '1';
+        }, 250);
+    });
+}
 
     // method for listening to which sign button was clicked
-    app.getSignButtons = () => {
+app.getSignButtons = () => {
 
-        // method to display sign info when user hovers over symbol
-        app.signsButtons.forEach((button)=>{
-            button.addEventListener('mouseover', function() {
-                let sign = this.attributes.id.textContent;
-                app.signInfo = document.getElementById(`${sign}Info`);
-                app.signInfo.style.transform = "scale(1)";
-            })
+    //depending in which was clicked, give that as input to the api and changes the screen to horoscope view
+    app.signsButtons.forEach((button) => {
+        button.addEventListener('click', function () {
+            app.apiSign = this.attributes.id.textContent;
+            app.signsViewSection.style.opacity = '0';
+            // call api fetching method
+            app.getHoroscope();
+            app.changeSign();
+            
+            // gives opacity transition time, then switch views
+            setTimeout(function(){
+                app.signsViewSection.style.display='none';
+                app.horoscopeSection.style.display='block';
+                app.changeButton.style.visibility='visible';
+                app.changeButton.style.opacity = '1';
+                app.horoscopeSection.style.opacity = '1';
+            }, 250);
         });
+    });
+};
 
-        // method to hide sign info when user hovers over symbol
-        app.signsButtons.forEach((button) => {
-            button.addEventListener('mouseout', function () {
-                let sign = (this.attributes.id.textContent);
-                app.signInfo = document.getElementById(`${sign}Info`);
-                app.signInfo.style.transform = "scale(0)";
-            })
-        });
+app.init = () => {
+    app.signsButtons = document.querySelectorAll('.signsButton');
+    app.changeButton = document.querySelector('.changeButton');
+    app.horoscopeSection = document.querySelector('.horoscopeViewSection');
+    app.signsViewSection = document.querySelector('.signButtonSection');
 
-        // method to display sign info when user hovers over symbol
-        app.signsButtons.forEach((button) => {
-            button.addEventListener('focusin', function () {
-                let sign = this.attributes.id.textContent;
-                app.signInfo = document.getElementById(`${sign}Info`);
-                app.signInfo.style.transform = "scale(1)";
-            })
-        });
+    app.apiSelection = document.querySelector('.apiSelection');
+    app.apiDateRange = document.querySelector('.apiDateRange');
+    app.apiCurrentDate = document.querySelector('.apiCurrentDate');
+    app.apiDescription = document.querySelector('.apiDescription');
+    app.apiCompatibility = document.querySelector('.apiCompatibility');
+    app.apiMood = document.querySelector('.apiMood');
+    app.apiColor = document.querySelector('.apiColor');
+    app.apiLuckyNumber = document.querySelector('.apiLuckyNumber');
+    app.apiLuckyTime = document.querySelector('.apiLuckyTime');
 
-        // method to hide sign info when user hovers over symbol
-        app.signsButtons.forEach((button) => {
-            button.addEventListener('focusout', function () {
-                let sign = (this.attributes.id.textContent);
-                app.signInfo = document.getElementById(`${sign}Info`);
-                app.signInfo.style.transform = "scale(0)";
-            })
-        });
+    app.apiImage = document.getElementById('apiImage');
 
-        // method to iterate through all the sign buttons and listen to which one was clicked. Then changes the screen to that horoscope view
-        app.signsButtons.forEach((button) => {
-            button.addEventListener('click', function () {
-                app.horoscopeSection = document.querySelector('.horoscopeViewSection');
-                app.signsViewSection = document.querySelector('.signButtonSection');
-                app.apiSign = this.attributes.id.textContent;
-                console.log("here", app.apiSign);
-                app.signsViewSection.style.opacity = "0";
-                app.getHoroscope();
-                app.changeSign();
-                
-                setTimeout(function(){
+    app.apiURL = 'https://aztro.sameerkumar.website';
 
-                    app.signsViewSection.style.display="none";
-                    app.horoscopeSection.style.display='block';
-                    app.changeButton.style.visibility="visible";
-                    app.changeButton.style.opacity = "1";
-                    app.horoscopeSection.style.opacity = "1";
-                }, 250);
+    app.apiSign = ''; // based on user input
+    app.apiDay = 'today';
 
-            });
-        });
-     };
+    app.hovers();
+    app.getSignButtons();
+}
 
-    app.init = () => {
-        app.signsButtons = document.querySelectorAll('.signsButton');
-        app.changeButton = document.querySelector('.homeButton');
-        app.getSignButtons();
-    }
-    
-        app.init();
-
-
-
+app.init();
 
 
 // create an app object
