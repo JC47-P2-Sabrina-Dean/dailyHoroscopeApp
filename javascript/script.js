68eaadf47669608c8620bd2732163dc3f996bcd9
SabrinaIsTheMan <sabrina.yt.man@gmail.com>
2023-03-11 19:32:52 -0500

    three api calls, struggling with toggleCard method

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,10 @@
 const app = {};
 
-app.getHoroscope = () => {
+app.getHoroscopeToday = () => {
     const url = new URL(app.apiURL);
     url.search = new URLSearchParams({
         sign: app.apiSign,
-        day: app.apiDay
+        day: 'today'
     });
 
     fetch(url, { method: 'POST' })
@@ -13,7 +13,7 @@ app.getHoroscope = () => {
             if (response.ok === true) { 
                 return response.json();
             } else {
-                throw new Error(response.statusText); // 
+                throw new Error(response.statusText);
             }
         })
         .then((jsonResult) => {
@@ -21,21 +21,83 @@ app.getHoroscope = () => {
 
             app.apiImage.attributes.src.textContent = `./assets/${app.apiSign}.png`;
             app.apiImage.attributes.alt.textContent = `${app.apiSign} symbol`;
-
             app.apiSelection.innerText = app.apiSign;
             app.apiDateRange.innerText = jsonResult.date_range;
-
             app.apiCurrentDate.innerText = jsonResult.current_date;
-            app.apiDescription.innerText = jsonResult.description;
 
-            app.apiCompatibility.innerText = jsonResult.compatibility;
-            app.apiMood.innerText = jsonResult.mood;
-            app.apiColor.innerText = jsonResult.color;
-            app.apiLuckyNumber.innerText = jsonResult.lucky_number;
-            app.apiLuckyTime.innerText = jsonResult.lucky_time;
+            app.apiDescriptionToday.innerText = jsonResult.description;
+            app.apiCompatibilityToday.innerText = jsonResult.compatibility;
+            app.apiMoodToday.innerText = jsonResult.mood;
+            app.apiColorToday.innerText = jsonResult.color;
+            app.apiNumberToday.innerText = jsonResult.lucky_number;
+            app.apiTimeToday.innerText = jsonResult.lucky_time;
         })
         .catch ((error) => {
-            alert("Something went wrong, try again later");
+            alert("Something went wrong, try again later (1)");
+            window.location.reload(true);
+        })
+};
+
+app.getHoroscopeYesterday = () => {
+    const url = new URL(app.apiURL);
+    url.search = new URLSearchParams({
+        sign: app.apiSign,
+        day: 'yesterday'
+    });
+
+    fetch(url, { method: 'POST' })
+        .then((response) => {
+            console.log(response);
+            if (response.ok === true) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText); // 
+            }
+        })
+        .then((jsonResult) => {
+            console.log(jsonResult);
+
+            app.apiDescriptionYesterday.innerText = jsonResult.description;
+            app.apiCompatibilityYesterday.innerText = jsonResult.compatibility;
+            app.apiMoodYesterday.innerText = jsonResult.mood;
+            app.apiColorYesterday.innerText = jsonResult.color;
+            app.apiNumberYesterday.innerText = jsonResult.lucky_number;
+            app.apiTimeYesterday.innerText = jsonResult.lucky_time;
+        })
+        .catch((error) => {
+            alert("Something went wrong, try again later (0)");
+            window.location.reload(true);
+        })
+};
+
+app.getHoroscopeTomorrow = () => {
+    const url = new URL(app.apiURL);
+    url.search = new URLSearchParams({
+        sign: app.apiSign,
+        day: 'tomorrow'
+    });
+
+    fetch(url, { method: 'POST' })
+        .then((response) => {
+            console.log(response);
+            if (response.ok === true) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText); // 
+            }
+        })
+        .then((jsonResult) => {
+            console.log(jsonResult);
+
+            app.apiDescriptionTomorrow.innerText = jsonResult.description;
+            app.apiCompatibilityTomorrow.innerText = jsonResult.compatibility;
+            app.apiMoodTomorrow.innerText = jsonResult.mood;
+            app.apiColorTomorrow.innerText = jsonResult.color;
+            app.apiNumberTomorrow.innerText = jsonResult.lucky_number;
+            app.apiTimeTomorrow.innerText = jsonResult.lucky_time;
+        })
+        .catch((error) => {
+            alert("Something went wrong, try again later (2)");
             window.location.reload(true);
         })
 };
@@ -80,15 +142,15 @@ app.hovers = () => {
 
 // method to change back to the sign button selection screen
 app.changeSign = () => {
-    app.changeButton.addEventListener('click', function () {
+    app.backButton.addEventListener('click', function () {
         console.log('click');
-        app.changeButton.style.opacity = '0';
+        app.backButton.style.opacity = '0';
         app.horoscopeSection.style.opacity = '0';
 
         // gives opacity transition time, then switch views back
         setTimeout(function() {
             app.horoscopeSection.style.display = 'none';
-            app.changeButton.style.visibility = 'hidden';
+            app.backButton.style.visibility = 'hidden';
             app.signsViewSection.style.display = 'block';
             app.signsViewSection.style.opacity = '1';
         }, 250);
@@ -103,47 +165,84 @@ app.getSignButtons = () => {
         button.addEventListener('click', function () {
             app.apiSign = this.attributes.id.textContent;
             app.signsViewSection.style.opacity = '0';
-            // call api fetching method
-            app.getHoroscope();
+            // call api fetching methods
+            app.getHoroscopeYesterday();
+            app.getHoroscopeToday();
+            app.getHoroscopeTomorrow();
             app.changeSign();
             
             // gives opacity transition time, then switch views
             setTimeout(function(){
                 app.signsViewSection.style.display='none';
                 app.horoscopeSection.style.display='block';
-                app.changeButton.style.visibility='visible';
-                app.changeButton.style.opacity = '1';
+                app.backButton.style.visibility='visible';
+                app.backButton.style.opacity = '1';
                 app.horoscopeSection.style.opacity = '1';
             }, 250);
         });
     });
 };
 
+
+app.toggleHoroscope = () => {
+    function toggleCards() {
+        // app.horoscopePanels.classList.remove('open');
+        // app.horoscopePanels.children.classList.remove('open');
+        this.classList.toggleClass('open');
+        this.children.classList.toggle('open');  
+    };
+
+    app.horoscopePanels.forEach((panel) => {
+        console.log('hello');
+        panel.addEventListener('click', toggleCards());
+    });
+};
+
 app.init = () => {
     app.signsButtons = document.querySelectorAll('.signsButton');
-    app.changeButton = document.querySelector('.changeButton');
+    app.backButton = document.querySelector('.backButton');
     app.horoscopeSection = document.querySelector('.horoscopeViewSection');
     app.signsViewSection = document.querySelector('.signButtonSection');
+    app.horoscopeBox = document.querySelector('.horoscopeView');
+    app.horoscopePanels = document.querySelectorAll('.horoscopeFlex');
+    app.apiFacts = document.querySelector('.apiFacts');
 
+    app.apiImage = document.querySelector('.apiImage');
     app.apiSelection = document.querySelector('.apiSelection');
     app.apiDateRange = document.querySelector('.apiDateRange');
     app.apiCurrentDate = document.querySelector('.apiCurrentDate');
-    app.apiDescription = document.querySelector('.apiDescription');
-    app.apiCompatibility = document.querySelector('.apiCompatibility');
-    app.apiMood = document.querySelector('.apiMood');
-    app.apiColor = document.querySelector('.apiColor');
-    app.apiLuckyNumber = document.querySelector('.apiLuckyNumber');
-    app.apiLuckyTime = document.querySelector('.apiLuckyTime');
 
-    app.apiImage = document.getElementById('apiImage');
+    // today
+    app.apiDescriptionToday = document.querySelector('.apiDescriptionToday');
+    app.apiCompatibilityToday = document.querySelector('.apiCompatibilityToday');
+    app.apiMoodToday = document.querySelector('.apiMoodToday');
+    app.apiColorToday = document.querySelector('.apiColorToday');
+    app.apiNumberToday = document.querySelector('.apiLuckyNumberToday');
+    app.apiTimeToday = document.querySelector('.apiLuckyTimeToday');
 
+    // yesterday
+    app.apiDescriptionYesterday = document.querySelector('.apiDescriptionYesterday');
+    app.apiCompatibilityYesterday = document.querySelector('.apiCompatibilityYesterday');
+    app.apiMoodYesterday = document.querySelector('.apiMoodYesterday');
+    app.apiColorYesterday = document.querySelector('.apiColorYesterday');
+    app.apiNumberYesterday = document.querySelector('.apiLuckyNumberYesterday');
+    app.apiTimeYesterday = document.querySelector('.apiLuckyTimeYesterday');
+
+    // tomorrow
+    app.apiDescriptionTomorrow = document.querySelector('.apiDescriptionTomorrow');
+    app.apiCompatibilityTomorrow = document.querySelector('.apiCompatibilityTomorrow');
+    app.apiMoodTomorrow = document.querySelector('.apiMoodTomorrow');
+    app.apiColorTomorrow = document.querySelector('.apiColorTomorrow');
+    app.apiNumberTomorrow = document.querySelector('.apiLuckyNumberTomorrow');
+    app.apiTimeTomorrow = document.querySelector('.apiLuckyTimeTomorrow');
+    
     app.apiURL = 'https://aztro.sameerkumar.website';
 
     app.apiSign = ''; // based on user input
-    app.apiDay = 'today';
 
     app.hovers();
     app.getSignButtons();
+    app.toggleHoroscope();
 }
 
 app.init();
@@ -161,7 +260,7 @@ app.init();
 
 // displayHoroscope method
 	// query select right div/elements
-	
+
 	// create new elements and append as children
 	// OR
 	// update innerText
@@ -177,3 +276,11 @@ app.init();
 	// init the horoscope image method
 
 // initialize
+
+
+
+// on click
+    // add .openActive to clicked
+    // take off .openActive for others
+    // writing-mode: horizontal-tb
+    // display: block
